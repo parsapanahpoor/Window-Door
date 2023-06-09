e017f249a481baa3badd91de5afebaa221ec4c43
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-06-09 10:24:40 +0330

    start for bulk sms

---
+++ Window.Domain/Entities/BulkSMS/BulkSMS.cs
@@ -0,0 +1,20 @@
+﻿#region Usings
+
+using Window.Domain.Entities.Common;
+namespace Window.Domain.Entities.BulkSMS;
+
+#endregion
+
+
+public sealed class BulkSMS : BaseEntity
+{
+	#region properties
+
+	public string Mobile { get; set; }
+
+	public string Username { get; set; }
+
+	public bool IsSent { get; set; }
+
+	#endregion
+}
