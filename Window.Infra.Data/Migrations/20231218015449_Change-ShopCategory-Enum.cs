343bb0a100fe3fc89b1d76012da60cb2737760e1
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-12-18 05:55:33 +0330

    clean up layout

---
+++ Window.Infra.Data/Migrations/20231218015449_Change-ShopCategory-Enum.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Window.Infra.Data.Migrations
+{
+    public partial class ChangeShopCategoryEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 1m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6255));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 2m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6265));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 3m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6272));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 4m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6279));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 5m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6286));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6205));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6223));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6231));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 18, 5, 24, 48, 588, DateTimeKind.Local).AddTicks(6238));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 1m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9173));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 2m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9185));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 3m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9193));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 4m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9201));
+
+            migrationBuilder.UpdateData(
+                table: "ProductTypeCategories",
+                keyColumn: "Id",
+                keyValue: 5m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9209));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9107));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9133));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9142));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4m,
+                column: "CreateDate",
+                value: new DateTime(2023, 12, 12, 0, 58, 58, 679, DateTimeKind.Local).AddTicks(9150));
+        }
+    }
+}
